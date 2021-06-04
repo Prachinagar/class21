82ca6bf23100d334f5048aa6370bf0b1edbbde75
prachi nagar <78290902+Prachinagar@users.noreply.github.com>
2021-06-04 16:02:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  rectangle = createSprite(50,200,30,30);
+
+
+
+  
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,rectangle)){
+    movingRect.shapeColor = "blue";
+    rectangle.shapeColor = "blue";
+  }
+  
+  else {
+    movingRect.shapeColor = "green";
+  
+  rectangle.shapeColor = "green"
+  }
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
+}
+
+
+
+
